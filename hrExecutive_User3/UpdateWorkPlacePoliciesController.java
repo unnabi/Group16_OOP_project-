23f6abcb7ca0a5c7e40de97456240a704fb17044
Sajid-UlIslam <2222219@iub.edu.bd>
2024-04-07 16:23:50 +0600

    second commit
    HR executives 3 goals

--- hrExecutive_User3/UpdateWorkPlacePoliciesController.java
+++ hrExecutive_User3/UpdateWorkPlacePoliciesController.java
@@ -4,8 +4,11 @@
  */
 package hrExecutive_User3;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,21 +23,32 @@ public class UpdateWorkPlacePoliciesController implements Initializable {
 
     @FXML
     private TextArea policyTextArea;
+    
+    Policy policy;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+            // TODO4
+            policy = new Policy();
+            String policyText;
+            policyText = policy.loadPolicy();
+            policyTextArea.setText(policyText);
+        
     }    
 
     @FXML
-    private void updatePolicyButtonOnMouseClick(ActionEvent event) {
+    private void updatePolicyButtonOnMouseClick(ActionEvent event) throws IOException {
+        String policyText = policyTextArea.getText();
+        policy.savePolicy(policyText);
     }
 
     @FXML
-    private void loadPolicyButtonOnMouseClick(ActionEvent event) {
+    private void loadPolicyButtonOnMouseClick(ActionEvent event) throws IOException {
+        String policyText = policy.loadPolicy();
+        policyTextArea.setText(policyText);
     }
     
 }
