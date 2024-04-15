f93fa5b38b3a67e15e6acedd43d2c6f5e19c1b55
Md.Shehab-Un-Nabi <157971032+unnabi@users.noreply.github.com>
2024-04-15 10:00:50 +0600

    1st commit
    build  register form to sign up as a retailer.

---
+++ ceramicindustry_oopproject_group16/FXMLDocumentController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ceramicindustry_oopproject_group16;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author SHEHAB NABI
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private TextField TFfirstname;
+    @FXML
+    private TextField TFlastname;
+    @FXML
+    private TextField TFemail;
+    @FXML
+    private TextField TFaddress;
+    @FXML
+    private TextField TFnumber;
+    @FXML
+    private TextField TFcompany;
+    @FXML
+    private TextField TFsign;
+    @FXML
+    private TextField TFshowResult;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void RegisterOnMouseClicked(ActionEvent event) {
+        String FirstName = TFfirstname.getText();
+        String LastName = TFlastname.getText();
+        
+        
+        
+    }
+    
+}
